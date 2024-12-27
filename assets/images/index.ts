8f6eaef9496622fe3d3d7284673c37df659dce56
odafe <godfreyj.sule1@gmail.com>
2024-12-27 22:23:46 +0100

    new

---
+++ assets/images/index.ts
@@ -0,0 +1,17 @@
+export { ReactComponent as LogoIcon } from "./logobasket.svg";
+export { ReactComponent as Handshake } from "./handshake.svg";
+export { ReactComponent as PruchaseIcon } from "./Purchase.svg";
+export { ReactComponent as CostIcon } from "./Cost.svg";
+export { ReactComponent as CancelledIcons } from "./Cancel.svg";
+export { ReactComponent as ReturnedIcon } from "./return.svg";
+export { ReactComponent as SalesIcon } from "./Sales.svg";
+export { ReactComponent as RevenueIcon } from "./Revenue.svg";
+export { ReactComponent as QuanityIcon } from "./Quantity.svg";
+export { ReactComponent as OnTheWayIcon } from "./OnTheWay.svg";
+export { ReactComponent as Chips } from "./chips.svg";
+export { ReactComponent as ChipsOne } from "./Chip1.svg";
+export { ReactComponent as Fruits } from "./Fruits.svg";
+export { ReactComponent as Veggie } from "./Veggie.svg";
+export { ReactComponent as Fish } from "./Fish.svg";
+export { ReactComponent as Meat } from "./Meat.svg";
+export { ReactComponent as Dairy } from "./Dairy.svg";
